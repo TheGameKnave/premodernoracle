abd1ed7fcd28ed0de03add973718cc1320396897
GameKnave <git@gameknave.com>
2024-01-24 02:25:43 -0800

    home styling

--- frontend/src/views/home.vue
+++ frontend/src/views/home.vue
@@ -1,28 +1,22 @@
-
 <template>
   <div>
-    <textarea
-      v-model="cardNames"
-      placeholder="Enter card names, one per line"
-    />
-    <button @click="fetchCards">Submit</button>
+    <form>
+      <textarea
+        v-model="cardNames"
+        placeholder="Enter card names, one per line"
+      />
+      <button type="button" @click="fetchCards">Submit</button>
+    </form>
 
     <template v-if="Object.keys(cardData).length">
       <h2>Cards</h2>
-      <div 
-        v-if="cardData.length === 0" 
-        class="error">No cards found</div>
-      <div 
-        v-if="missingCards.length > 0" 
-        class="error">
+      <div v-if="cardData.length === 0" class="error">No cards found</div>
+      <div v-if="missingCards.length > 0" class="error">
         Missing cards: {{ missingCards.join(', ') }}
       </div>
       <div class="cards">
         <template v-for="(card, key) in cardData">
-          <card 
-            v-if="card.name" 
-            :key="key" 
-            :card="card"/>
+          <card v-if="card.name" :key="key" :card="card" />
         </template>
       </div>
     </template>
@@ -30,58 +24,103 @@
 </template>
 
 <script>
-  import Card from '@/components/card.vue';
-  export default {
-    components: {
-      Card
-    },
-    data() {
-      return {
-        cardNames: '', // Initialize the data property for the textarea value
-        cardData: [],
-        missingCards: [],
+import Card from '@/components/card.vue'
+export default {
+  components: {
+    Card,
+  },
+  data() {
+    return {
+      cardNames: '', // Initialize the data property for the textarea value
+      cardData: [],
+      missingCards: [],
+    }
+  },
+  computed: {},
+  mounted() {
+    // Access and adjust the size of the element after the component has been mounted
+    this.adjustTextboxSize()
+  },
+  methods: {
+    adjustTextboxSize() {
+      let textarea = document.getElementsByTagName('textarea')[0]
+      if (textarea) {
+        textarea.addEventListener('input', () => {
+          textarea.style.height = 'auto'
+          textarea.style.height = textarea.scrollHeight + 'px'
+        })
+
+        // Initialize the textarea height
+        textarea.style.height = textarea.scrollHeight + 'px'
       }
     },
-    computed: {},
-    methods: {
-      // method to fetch cards based on form contents
-      async fetchCards() {
-        let cardList = this.cardNames.split(/\r?\n/)
-        // remove any leading numbers and spaces, and any trailing spaces
-        cardList = cardList
-          .map(card => card.replace(/^\d+|\s+$/g, '').trim())
-          .filter(card => card)
-        // dedup the list
-        cardList = cardList.filter(
-          (card, index) => cardList.indexOf(card) === index
-        )
-        this.cardNames = cardList.join('\n')
-        fetch('/api/cards', {
-          method: 'POST', // Update the method to 'POST'
-          headers: {
-            'Content-Type': 'application/json', // Set the content type header if sending JSON payload
-          },
-          body: JSON.stringify({ cardList: cardList }), // Include your payload data here
+    // method to fetch cards based on form contents
+    async fetchCards() {
+      let cardList = this.cardNames.split(/\r?\n/)
+      // remove any leading numbers and spaces, and any trailing spaces
+      cardList = cardList
+        .map(card => card.replace(/^\d+|\s+$/g, '').trim())
+        .filter(card => card)
+      // dedup the list
+      cardList = cardList.filter(
+        (card, index) => cardList.indexOf(card) === index
+      )
+      this.cardNames = cardList.join('\n')
+      fetch('/api/cards', {
+        method: 'POST', // Update the method to 'POST'
+        headers: {
+          'Content-Type': 'application/json', // Set the content type header if sending JSON payload
+        },
+        body: JSON.stringify({ cardList: cardList }), // Include your payload data here
+      })
+        .then(response => response.json())
+        .then(cards => {
+          this.cardData = cards
+          this.missingCards = Object.entries(cards)
+            .filter(([key, value]) => !Object.keys(value).length)
+            .map(([key, value]) => key)
         })
-          .then(response => response.json())
-          .then(cards => {
-            this.cardData = cards
-            this.missingCards = Object.entries(cards)
-              .filter(([key, value]) => !Object.keys(value).length)
-              .map(([key, value]) => key)
-          })
-          .catch(error => {
-            console.error('Fetch error:', error)
-          });
-      },
+        .catch(error => {
+          console.error('Fetch error:', error)
+        })
     },
-  }
+  },
+}
 </script>
 
-
-<style scoped>
-  .cards {
-    display: flex;
-    flex-wrap: wrap;
-  }
+<style>
+@font-face {
+  font-family: 'Magic';
+  font-style: normal;
+  font-weight: 100;
+  src: url('~@/assets/fonts/Goudy Medieval Alternate.ttf');
+}
+/* magic text */
+@font-face {
+  font-family: 'Plantin';
+  font-style: normal;
+  font-weight: 100;
+  src: url('~@/assets/fonts/Plantin.otf');
+}
+textarea,
+h2 {
+  font-family: 'Magic';
+}
+form {
+  display: flex;
+}
+textarea {
+  flex-grow: 1;
+  height: auto;
+  max-height: 50vh;
+  font-size: 18px;
+}
+button,
+.error {
+  font-family: 'Plantin';
+}
+.cards {
+  display: flex;
+  flex-wrap: wrap;
+}
 </style>
